bdc3fe27a36abcdfb836151adfd6bf30b7ddb13f
Zhakshylykov-Murat <85300145+Zhakshylykov-Murat@users.noreply.github.com>
2021-07-09 17:10:11 +0600

    Add files via upload

---
+++ less 31/src/js/main.js
@@ -0,0 +1,34 @@
+let button = document.getElementById( 'finish' )
+
+button.addEventListener( 'click' , checkTest )
+
+function checkTest () {
+    let result = 0
+
+    let a1 = document.getElementById ( 'q1' ).value
+    if ( a1 === '4' ) {
+        result++
+    }
+
+    let a2 = document.getElementById ( 'q2' ).value
+    if ( a2 === '25' ) {
+        result++
+    }
+
+    let a3 = document.getElementById ( 'q3' ).value
+    if ( a3 === '4' ) {
+        result++
+    }
+
+    let a4 = document.getElementById ( 'q4' ).value
+    if ( a4 === '5' ) {
+        result++
+    }
+
+    let a5 = document.getElementById ( 'q5' ).value
+    if ( a5 === '40' ) {
+        result++
+    }
+
+    alert( 'правельных ответов: ' + result)
+}
